aeb4546fec9d9abc32d984393908c37a8e50a06b
oso179 <141197064+oso179@users.noreply.github.com>
2023-08-02 10:39:22 +0800

    R file.R

--- R file.R
+++ R file.R
@@ -2,7 +2,7 @@
 If(!file.exists("./data")){dir.create("./data")}
 fileUrl <- https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
 download.file(fileUrl, destfile="./data/activities.zip",method="curl")
-
+ 
 #Unzip file
 If(!file.exists("UCI HAR Dataset")){unzip("activities.zip"}
 
